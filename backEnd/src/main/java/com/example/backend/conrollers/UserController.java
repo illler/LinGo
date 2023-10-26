e93fd9bc505547c307ea574bfddfd54a60417d5f
Alexandr Pyatunin <0250901@gmail.com>
2023-10-26 16:00:02 +0300

    added method to get User

--- backEnd/src/main/java/com/example/backend/conrollers/UserController.java
+++ backEnd/src/main/java/com/example/backend/conrollers/UserController.java
@@ -7,13 +7,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/profile")
+@RequestMapping("/api/v1/")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserRepository userRepository;
 
-    @PutMapping("/saveUserInfo")
+    @GetMapping("/getCurrentUser")
+    public ResponseEntity<User> getCurrentUser(@RequestBody String token){
+        return ResponseEntity.ok(userRepository.findUsersByTokens(token).orElse(null));
+    }
+
+
+    @PutMapping("profile/saveUserInfo")
     public ResponseEntity<User> saveUserInfo( @RequestBody User user){
         userRepository.save(user);
         return ResponseEntity.ok(user);
--- backEnd/src/main/java/com/example/backend/repositories/UserRepository.java
+++ backEnd/src/main/java/com/example/backend/repositories/UserRepository.java
@@ -11,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByEmail(String email);
 
+    Optional<User> findUsersByTokens(String token);
+
 }
